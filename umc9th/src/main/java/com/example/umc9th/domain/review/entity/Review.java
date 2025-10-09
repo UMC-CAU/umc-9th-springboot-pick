7759d68fe28ad3dd0da234f368712eb8f901efca
sseobi16 <jseobi3951@naver.com>
2025-10-09 15:22:18 +0900

    feat: review 도메인 entity 매핑 및 연관 관계 설정

---
+++ umc9th/src/main/java/com/example/umc9th/domain/review/entity/Review.java
@@ -0,0 +1,37 @@
+package com.example.umc9th.domain.review.entity;
+
+import com.example.umc9th.domain.member.entity.Member;
+import com.example.umc9th.domain.store.entity.Store;
+import com.example.umc9th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = " review")
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "star_score", precision = 2, scale = 1, nullable = false)
+    private BigDecimal star_score = BigDecimal.ZERO;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
