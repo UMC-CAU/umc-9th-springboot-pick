c4fbbfc8267f5244335f0f2c8098f118377cd616
sseobi16 <jseobi3951@naver.com>
2025-10-31 02:56:43 +0900

    feat: 미션 조회 쿼리 리펙토링

---
+++ umc9th/src/main/java/com/example/umc9th/domain/mission/dto/MissionShowDto.java
@@ -0,0 +1,3 @@
+package com.example.umc9th.domain.mission.dto;
+
+public record MissionShowDto(Integer score, String storeName, String todo) { }
--- umc9th/src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
+++ umc9th/src/main/java/com/example/umc9th/domain/mission/repository/MissionRepository.java
@@ -1,7 +1,46 @@
 package com.example.umc9th.domain.mission.repository;
 
 import com.example.umc9th.domain.mission.entity.Mission;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.umc9th.domain.mission.dto.MissionShowDto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+    @Query("""
+        select new com.example.umc9th.domain.mission.dto.MissionShowDto(m.score, s.name, m.todo)
+        from UserMission um
+        join um.mission m
+        join m.store s
+        where um.member.id = :memberId
+          and um.isFinished = true
+          and (:cursorId is null or um.id < :cursorId)
+        order by um.id desc
+    """)
+    List<MissionShowDto> findCompletedMissions(
+            @Param("memberId") Long memberId,
+            @Param("cursorId") Long cursorId,
+            Pageable pageable
+    );
+
+    @Query("""
+        select new com.example.umc9th.domain.mission.dto.MissionShowDto(m.score, s.name, m.todo)
+        from UserMission um
+        join um.mission m
+        join m.store s
+        where um.member.id = :memberId
+          and um.isFinished = false
+          and (:cursorId is null or um.id < :cursorId)
+        order by um.id desc
+    """)
+    List<MissionShowDto> findOngoingMissions(
+            @Param("memberId") Long memberId,
+            @Param("cursorId") Long cursorId,
+            Pageable pageable
+    );
+
 }
