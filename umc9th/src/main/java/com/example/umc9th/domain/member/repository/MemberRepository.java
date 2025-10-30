9c02500e0016bd67fa435cdc575ea38ddb8c324b
sseobi16 <jseobi3951@naver.com>
2025-10-31 02:43:42 +0900

    feat: 마이페이지 조회 쿼리 리펙토링

--- umc9th/src/main/java/com/example/umc9th/domain/member/repository/MemberRepository.java
+++ umc9th/src/main/java/com/example/umc9th/domain/member/repository/MemberRepository.java
@@ -4,4 +4,5 @@ import com.example.umc9th.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByIdAndDeletedAtIsNull(Long id);
 }
