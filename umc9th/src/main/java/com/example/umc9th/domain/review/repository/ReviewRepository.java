bc9711e638f9909d880628116ceb12bed9c2ba8a
sseobi16 <jseobi3951@naver.com>
2025-10-31 02:23:42 +0900

    feat: 리뷰 작성 쿼리 리펙토링

--- umc9th/src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
+++ umc9th/src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
@@ -7,6 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = """
+        INSERT INTO review (content, star_score, created_at, member_id, store_id)"
+        VALUES (:content, :starScore, NOW(), :memberId, :storeId)
+    """, nativeQuery = true)
+    int insertNative(@Param("content") String content,
+                     @Param("starScore") String starScore, // DECIMAL(2,1) → "4.5" 형태 문자열 or BigDecimal.toPlainString()
+                     @Param("memberId") Long memberId,
+                     @Param("storeId") Long storeId);
 
 }
