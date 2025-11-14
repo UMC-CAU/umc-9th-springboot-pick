cccab3e7f61ee46bc284b28084bc3e68dad31170
sseobi16 <jseobi3951@naver.com>
2025-11-14 22:43:19 +0900

    feat: Chapter7 미션 - api response 통일

--- umc9th/src/main/java/com/example/umc9th/domain/review/controller/MyReviewController.java
+++ umc9th/src/main/java/com/example/umc9th/domain/review/controller/MyReviewController.java
@@ -2,6 +2,8 @@ package com.example.umc9th.domain.review.controller;
 
 import com.example.umc9th.domain.review.dto.MyReviewDto;
 import com.example.umc9th.domain.review.service.MyReviewService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +17,14 @@ public class MyReviewController {
     private final MyReviewService service;
 
     @GetMapping
-    public List<MyReviewDto> getMyReviews(
+    public ApiResponse<List<MyReviewDto>> getMyReviews(
             // 실제 운영에선 @AuthenticationPrincipal 로부터 memberId 추출 권장
             @RequestHeader("X-MEMBER-ID") Long memberId,
             @RequestParam(required = false) String storeName, // 부분 검색
             @RequestParam(required = false) Integer rating,   // 별점대 (5/4/3/2/1)
             @RequestParam(required = false) Integer limit       // 기본 15
     ) {
-        return service.getMyReviews(memberId, storeName, rating, limit);
+        List<MyReviewDto> result = service.getMyReviews(memberId, storeName, rating, limit);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, result);
     }
 }
